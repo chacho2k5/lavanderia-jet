591da40b64e39e61789dc582acee4cf4a8cd48d5
chacho2k5 <chacho2k5@gmail.com>
2022-07-04 00:31:43 -0300

    iniciando OT

--- app/Http/Livewire/Ots/OtCreate.php
+++ app/Http/Livewire/Ots/OtCreate.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Livewire\Ots;
 
+use App\Models\Articulo;
+use App\Models\Cliente;
 use Livewire\Component;
 
 class OtCreate extends Component
 {
+    public $clientes, $selectedCliente;
+    public $prendas, $selectedPrenda;
+
+    public $numero, $fecha_alta, $cliente_id, $estado_id, $entrega_hotel, $recibe_hotel;
+    public $entrega_lavanderia, $recibe_lavanderia, $observaciones;
+
+    public $recibe, $entrega;
+
+    public function mount() {
+        $this->clientes = Cliente::all();
+        $this->prendas = Articulo::all();
+
+    }
+
     public function render()
     {
         return view('livewire.ots.ot-create');
--- resources/views/livewire/ots/ot-create.blade.php
+++ resources/views/livewire/ots/ot-create.blade.php
@@ -13,16 +13,18 @@
     </div>
 
     <div class="card">
-        <div class="card-header">
-
+        <div class="card-header d-flex justify-content-between">
+            <div><b>FECHA: </b>{{ $fecha_alta }}</div>
+            <div><b>Nº </b>{{ $numero }}</div>
+            {{-- @json($clientes) --}}
         </div>
 
         <div class="card-body">
             <form wire:submit.prevent="submit">
-                <div class="form-group">
+            <div class="row gx-4 gy-1">
+                <div class="form-group col-md-2">
                     <label for="fecha_alta" class="form-label">Fecha OT</label>
-                    <input type="date" wire:model="fecha_alta" class="form-control mb-4">
-                    {{-- @error('name') <span class="error">{{ $message }}</span> @enderror --}}
+                    <input type="date" wire:model="fecha_alta" class="form-control">
                     @error('fecha_alta')
                         <span class="invalid-feedback" role="alert">
                             <span class="text-danger">{{ $message }}</span>
@@ -30,21 +32,105 @@
                     @enderror
                 </div>
 
-                <div class="form-group">
+                <div class="form-group col-md-2">
                     <label for="numero" class="form-label">Número OT</label>
-                    <input type="text" wire:model="numero" class="form-control mb-4">
-                    {{-- @error('name') <span class="error">{{ $message }}</span> @enderror --}}
+                    <input type="text" wire:model="numero" class="form-control">
                     @error('numero')
                         <span class="invalid-feedback" role="alert">
                             <span class="text-danger">{{ $message }}</span>
                         </span>
                     @enderror
                 </div>
+            </div>
 
-                <div class="form-group">
-                    <input type="text" wire:model="email" class="form-control mb-4">
-                    @error('email') <span class="error">{{ $message }}</span> @enderror
+            <div class="row">
+                <div class="form-group col-md-4">
+                    <label for="">Clientes</label>
+                    {{-- <div wire:ignore> --}}
+                    <select wire:model="selectedCliente" class="form-select">
+                        <option value="0">Seleccione un cliente</option>
+                        @foreach($clientes as $cliente)
+                            <option value="{{ $cliente->id }}">
+                                {{ $cliente->razonsocial }}
+                            </option>
+                        @endforeach
+                    </select>
                 </div>
+                <div class="form-group col-md-8">
+                    <label for="">Dirección del Cliente</label>
+                    <input type="text" class="form-control" value="{{ $clientes[2]['calle_nombre'] }}" placeholder="Dirección del cliente" disabled>
+                </div>
+            </div>
+
+                <div class="row mt-2">
+                    <div class="form-group col-md-5">
+                        <label for="entrega_hotel" class="form-label">Entrega Hotel</label>
+                        <input type="text" wire:model="entrega_hotel" class="form-control">
+                        @error('entrega_hotel')
+                            <span class="invalid-feedback" role="alert">
+                                <span class="text-danger">{{ $message }}</span>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-group col-md-5">
+                        <label for="recibe_lavanderia" class="form-label">Recibe Lavanderia</label>
+                        <input type="text" wire:model="recibe_lavanderia" class="form-control">
+                        @error('recibe_lavanderia')
+                            <span class="invalid-feedback" role="alert">
+                                <span class="text-danger">{{ $message }}</span>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="row mt-2">
+                    <div class="form-group col-md-5">
+                        <label for="entrega_lavanderia" class="form-label">Entrega Lavanderia</label>
+                        <input type="text" wire:model="entrega_lavanderia" class="form-control">
+                        @error('entrega_lavanderia')
+                            <span class="invalid-feedback" role="alert">
+                                <span class="text-danger">{{ $message }}</span>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-5">
+                        <label for="recibe_hotel" class="form-label">Recibe Hotel</label>
+                        <input type="text" wire:model="recibe_hotel" class="form-control">
+                        @error('recibe_hotel')
+                            <span class="invalid-feedback" role="alert">
+                                <span class="text-danger">{{ $message }}</span>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="form-group col-md-4">
+                        <label for="">Prendas</label>
+                        {{-- <div wire:ignore> --}}
+                        <select wire:model="selectedPrenda" class="form-select">
+                            <option value="0">Seleccione una prenda</option>
+                            @foreach($prendas as $prenda)
+                                <option value="{{ $prenda->id }}">
+                                    {{ $prenda->descripcion }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group col-md-1">
+                        <label for="">Recibe</label>
+                        <input wire:model="recibe" type="text" class="form-control">
+                    </div>
+                    <div class="form-group col-md-1">
+                        <label for="">Entrega</label>
+                        <input wire:model="entrega" type="text" class="form-control">
+                    </div>
+                    <div class="form-group col-md-2 mt-4">
+                        <button class="btn btn-success">Agregar prenda</button>
+                    </div>
+                </div>
+
 
                 <div class="mt-4 row d-print-none">
                     <div class="text-right col-12">
